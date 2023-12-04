32060cb5cee26daff173872de4fc21eb8e26de14
Proxx07 <zakir.pulatov97@gmail.com>
2023-12-04 17:51:00 +0500

    user role middleware activated

--- src/middlewares/checkUserRole.ts
+++ src/middlewares/checkUserRole.ts
@@ -1,11 +1,9 @@
 import {checkUserAccess} from "@/utils/roles.ts";
-import {USER_ID_KEY} from "@/models/staticContent/constants.ts";
-/*
+import {USER_ID_KEY, USER_ROLE} from "@/models/staticContent/constants.ts";
 import $axios from "@/api/axios.ts";
 import {AxiosResponse} from "axios";
 import {IResponse} from "@/models/interfaces/tableInterfaces.ts";
 import {IUserWithDate} from "@/models/interfaces/usersInterfaces.ts";
-*/
 
 export const checkUserRole = async (to: any) => {
   const userID = localStorage.getItem(USER_ID_KEY)
@@ -15,13 +13,10 @@ export const checkUserRole = async (to: any) => {
 
   else {
     if (!userID) return {name: 'auth'}
-    /*
-    const {data: {result}}: AxiosResponse<IResponse<IUserWithDate>> = await $axios.get(`/users/${userID}`)
-    if (!result.role) return {name: 'auth'}
+    const {data: {result}}: AxiosResponse<IResponse<IUserWithDate>> = await $axios.get('/auth/self')
+    if (!result.id) return {name: 'auth'}
     localStorage.setItem(USER_ROLE, `${result.role}`)
     checkUserAccess()
-    const roleName = getUserRoleName()
-    */
     if (checkUserAccess(to.meta.access)) return true
     return {name: "no-permission"}
   }

28ea970898c636e58218fbc4b48eb7932e9fc0bd
Proxx07 <zakir.pulatov97@gmail.com>
2023-12-06 14:26:47 +0500

    [DONE] Added build modes for prod and dev

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import {AUTH_TOKEN_NAME} from "@/models/staticContent/constants.ts";
 
+const URL: string = import.meta.env.MODE === 'production' ? 'https://faceid-admin.theable.tech' : 'https://faceid-admin-dev.theable.tech'
+
 const $axios = axios.create({
-  baseURL: 'https://faceid-admin-dev.theable.tech'
+  baseURL: URL
 });
+
 $axios.interceptors.request.use((config) => {
   const token = localStorage.getItem(AUTH_TOKEN_NAME);
   if (token) {

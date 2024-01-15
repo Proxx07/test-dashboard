156d4904a488c4e15764edec304701a327138994
Proxx07 <zakir.pulatov97@gmail.com>
2024-01-15 11:15:17 +0500

    [DONE] User checking refactoring

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,7 +3,7 @@ import $axios from "@/api/axios.ts";
 import {AxiosError} from "axios";
 import {AuthorizedUser, AuthUserInterface} from "@/models/auth/authUser.ts";
 import {IResponse} from "@/models/interfaces/tableInterfaces.ts";
-import {AUTH_TOKEN_NAME, USER_ID_KEY, USER_ROLE} from "@/models/staticContent/constants.ts";
+import {AUTH_TOKEN_NAME, USER_ROLE} from "@/models/staticContent/constants.ts";
 
 import {ref} from "vue";
 
@@ -23,14 +23,13 @@ export const useAuth = () => {
   const authSubmit = async () => {
     authUser.value.phone = authUser.value.phone.replace(/[^+\d]/g, '').substring(1);
     try {
-      const { data: {result, message} } = await $axios.post<IResponse<AuthorizedUser>>('auth/sign-in', authUser.value)
+      const { data: {result} } = await $axios.post<IResponse<AuthorizedUser>>('auth/sign-in', authUser.value)
 
       localStorage.setItem(AUTH_TOKEN_NAME, result.access_token)
-      localStorage.setItem(USER_ID_KEY, result.id)
       localStorage.setItem(USER_ROLE, `${result.role}`)
 
       await $router.push({path: '/'})
-      $toast.success(`Вы ${message.toLowerCase()} авторизоавлись`)
+      $toast.success(`Вы успешно авторизоавлись`)
     }
     catch (e) {
       const err = e as AxiosError<{message: string[]}>
@@ -49,7 +48,6 @@ export const useAuth = () => {
 
     if (!confirm('Вы уверены, что хотите выйти?')) return
     localStorage.removeItem(AUTH_TOKEN_NAME)
-    localStorage.removeItem(USER_ID_KEY)
     localStorage.removeItem(USER_ROLE)
     await $router.push('/auth')
   }
--- src/middlewares/checkUserRole.ts
+++ src/middlewares/checkUserRole.ts
@@ -2,18 +2,18 @@ import $axios from "@/api/axios.ts";
 import {IResponse} from "@/models/interfaces/tableInterfaces.ts";
 import {IUser} from "@/models/interfaces/usersInterfaces.ts";
 
-import {AUTH_TOKEN_NAME, USER_ID_KEY, USER_ROLE} from "@/models/staticContent/constants.ts";
+import {AUTH_TOKEN_NAME, USER_ROLE} from "@/models/staticContent/constants.ts";
 
 import {checkUserAccess} from "@/utils/roles.ts";
 
 export const checkUserRole = async (to: any) => {
-  const userID = localStorage.getItem(USER_ID_KEY)
+  const isUser = localStorage.getItem(AUTH_TOKEN_NAME)
   if (to.name === 'auth') {
-    if (userID) return {name: 'main'}
+    if (isUser) return {name: 'main'}
   }
 
   else {
-    if (!userID) return {name: 'auth'}
+    if (!isUser) return {name: 'auth'}
 
     try {
       const {data: {result}} = await $axios.get<IResponse<IUser>>('/auth/self')
@@ -24,7 +24,6 @@ export const checkUserRole = async (to: any) => {
     }
     catch (e) {
       localStorage.removeItem(AUTH_TOKEN_NAME)
-      localStorage.removeItem(USER_ID_KEY)
       localStorage.removeItem(USER_ROLE)
       return {name: 'auth'}
     }
